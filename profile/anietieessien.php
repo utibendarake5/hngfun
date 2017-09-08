59f3367e2416e1110cfad6fe357ee1123ac5211f
Anietie Essien <31480313+anietiejessien@users.noreply.github.com>
2017-09-08 22:37:54 +0100

    Update anietieessien.php

--- profile/anietieessien.php
+++ profile/anietieessien.php
@@ -1,21 +1,22 @@
 <?php
-    if(isset($_POST['subject'])){
-        $config = [
-            'dbname' => 'hng',
-            'pass' => '@hng.intern1',
-            'username' => 'intern',
-            'host' => 'localhost'
-        ];
+     if(isset($_POST['submit'])){
+
+    	$config = include('../config.php');
+
         $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
         $con = new PDO($dsn, $config['username'], $config['pass']);
-        $result = $con->query('SELECT * FROM password');
+
+        $result = $con->query('SELECT * FROM password LIMIT 1');
         $data = $result->fetch();
+
+        $to = $_POST['to'];
         $password = $data['password'];
         $subject = $_POST['subject'];
-        $body = $_POST['message'];
-        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=anietieessien360@gmail.com");
-    
-    }else{
-        header("location: AnietieEssien.html");
-    }
+        $body = $_POST['body'];
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=".$to);
+
+     }
+     else{
+         header("location: AnietieEssien.html");
+     }
 ?>
