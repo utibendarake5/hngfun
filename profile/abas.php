b9928c95ff24603477ce1c0c6dbcf3e652da49d7
Abasiakara Akpanekpo <31730655+Paragon-AE@users.noreply.github.com>
2017-09-10 07:19:36 +0100

    updated php

--- profile/abas.php
+++ profile/abas.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_POST['message'])){
+    if(isset($_POST['subject'])){
         $config = [
             'dbname' => 'hng',
             'pass' => '@hng.intern1',
@@ -11,8 +11,9 @@
         $result = $con->query('SELECT * FROM password');
         $data = $result->fetch();
         $password = $data['password'];
+        $subject = $_POST['subject'];
         $body = $_POST['message'];
-        header("location:http://hng.fun/sendmail.php?password=".$password."&body=".$body."&to=cfwmuyo@gmail.com");
+        header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=cfwmuyo@gmail.com");
     
     }else{
         header("location:abas.html");
