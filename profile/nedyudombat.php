071891b7401355685f74f6e94000d1aec549c6c8
Edidiong Udombat <nedyudombat@gmail.com>
2017-09-11 16:54:24 -0700

    contact form

--- profile/nedyudombat.php
+++ profile/nedyudombat.php
@@ -13,7 +13,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $error[] = 'Your name and email are very important, TYPE them in';
     }
     if(empty($error)) {
-        $config = include(dirname(dirname(dirname(__FILE__))).'/config.php');
+        // $config = include('../config.php');
+        $config = include(dirname(__FILE__).'/config.php');
         $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
         $con = new PDO($dsn, $config['username'], $config['pass']);
         $exe = $con->query('SELECT * FROM password LIMIT 1');
