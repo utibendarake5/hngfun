7e6cc4b8720e18960800cb5b6e57833fc8c37c2d
Edidiong Udombat <nedyudombat@gmail.com>
2017-09-11 10:31:57 -0700

    edited participants.php

--- participants.php
+++ participants.php
@@ -46,6 +46,29 @@ include('header.php');
                </div>
                <!--  Each Participants ends here  -->
 
+               <!--  Each Participant starts here  -->
+               <div class="p-card">
+                   <div class="v-center">
+                       <div class="p-card-photo" style="background-image: url(https://github.com/NedyUdombat/Nedy/blob/master/my%20d.p.jpg?raw=true); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+                       <div class="p-card-info">
+                           <a href="http://hng.fun/profile/nedyudombat.html">
+                               <!--Profile Link-->
+                               <h3 class="p-card-title">Edidiong Udombat</h3>
+                               <!--Profile Name-->
+                           </a>
+                           <p class="p-card-description">nedyudombat@gmail.com</p>
+                           <!--Email-->
+                       </div>
+                       <div class="p-team">
+                           <a href="#">
+                               <h3 class="p-card-title"> null </h3>
+                               <!--Team-->
+                           </a>
+                       </div>
+                   </div>
+               </div>
+               <!--  Each Participants ends here  -->
+
 <!--  Each Participant starts here  -->
 <div class="p-card">
     <div class="v-center">
