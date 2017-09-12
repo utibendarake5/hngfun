92811fffcaa5e262acce4a1132e3c892274bca82
Ndifreke Matthew Essien <31422222+Starlyvil@users.noreply.github.com>
2017-09-12 23:34:45 +0100

    Profile Page update

---
+++ profile/starlyvil/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Starlyvil's Profile | Basic info</title>
+<script type="text/javascript" src="js/jquery/jquery.js"></script>
+<script type="text/javascript" src="js/jquery/jqueryUI.js"></script>
+<link rel="stylesheet"  type="text/css" href="font/vicon/config.css"/>
+<script type="text/javascript" src="js/gen.js"></script>
+<link rel="stylesheet"  type="text/css" href="styles/gen.css"/>
+</head>
+
+<body>
+<div class="con">
+	<div class="cv"></div>
+	<div class="main">
+		<div class="lft">
+			<div class="tp">
+				<div class="pic">
+				</div>
+			</div>
+			<div class="bt">
+				<span>Starlyvil</span>
+			</div>
+		</div>
+		<div class="overlay">
+			<div class="spinner"></div>
+		</div>
+		<div class="rgt">
+			
+			
+		</div>
+	</div>
+	<div class="ft">
+		<div class="lCon">
+			<div class="mainCon">
+				<a id="Bi" data-target="binfo">Basic Info</a>
+				<a id="Ci" data-target="cinfo">Contact Info</a>
+				<a id="Sh" data-target="shandler">Social handler</a>
+				<a id="Cf" data-target="cform">Contact form</a>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
