bc07d90082ae66ab180084eaa81426a63d14580b
Iniobong <linibensonjr@gmail.com>
2017-09-12 20:34:43 +0100

    Updated Stage 3 profile task

--- profile/linibensonjr.php
+++ profile/linibensonjr.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_POST['subject'])){
+    if(isset($_POST['submit'])){
         $config = [
             'dbname' => 'hng',
             'pass' => '@hng.intern1',
@@ -12,7 +12,7 @@
         $data = $result->fetch();
         $password = $data['password'];
         $subject = $_POST['subject'];
-        $body = $_POST['message'];
+        $body = $_POST['body'];
         header("location:http://hng.fun/sendmail.php?password=".$password."&subject=".$subject."&body=".$body."&to=linibensonjr@gmail.com");
 	
     }else{
