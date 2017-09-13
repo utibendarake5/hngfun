cc74cbfd52c78db19b7428a5838764f7cae56a53
gbolahan_akande <hargae2011@gmail.com>
2017-09-13 10:10:09 +0100

    Making changes to the participants file

--- participants.php
+++ participants.php
@@ -6769,6 +6769,34 @@ include('header.php');
         </div>
         <!-- Each Participants ends here -->
 
+				<!--  Each Participant starts here  -->
+    <div class="p-card">
+        <div class="v-center">
+            <div class="p-card-photo" style="background-image: url(https://i.imgur.com/d5zrtI2.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; ">
+            <div class="p-card-info">
+                <a href="http://hng.fun/profile/gbolahan_akande.htm">
+                    <!--Profile Link-->
+                    <h3 class="p-card-title">Akande Gbolahan</h3>
+                    <!--Profile Name-->
+                </a>
+                <p class="p-card-description">hargae2011@gmail.com</p>
+                <!--Email-->
+            </div>
+            <div class="p-team">
+                <a href="#">
+                    <h3 class="p-card-title"> No Team </h3>
+                    <!--Team-->
+                </a>
+            </div>
+        </div>
+    </div>
+<!--  Each Participants ends here  -->
+		
+		
+		
+		
+		
+		
         <!--  Each Participant starts here  -->
         <div class="p-card">
             <div class="v-center">
@@ -23365,30 +23393,6 @@ include('participants-head.php');
             </div>
         </div>
         <!--  Each Participants ends here  -->
-
-		<!--  Each Participant starts here  -->
-    <div class="p-card">
-        <div class="v-center">
-            <div class="p-card-photo" style="background-image: url(https://i.imgur.com/d5zrtI2.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; ">
-            <div class="p-card-info">
-                <a href="http://hng.fun/profile/gbolahan_akande.htm">
-                    <!--Profile Link-->
-                    <h3 class="p-card-title">Akande Gbolahan</h3>
-                    <!--Profile Name-->
-                </a>
-                <p class="p-card-description">hargae2011@gmail.com</p>
-                <!--Email-->
-            </div>
-            <div class="p-team">
-                <a href="#">
-                    <h3 class="p-card-title"> No Team </h3>
-                    <!--Team-->
-                </a>
-            </div>
-        </div>
-    </div>
-<!--  Each Participants ends here  -->
-		
 		
 		
         <!--  Participant starts here  -->
