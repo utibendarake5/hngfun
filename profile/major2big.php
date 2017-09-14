6bb682604dd0bfd8d3009d172e2fc44b4ec882ee
Mayowa Micheal Fagbayi <31287355+Major2big@users.noreply.github.com>
2017-09-14 12:52:28 +0100

    Update major2big.php

--- profile/major2big.php
+++ profile/major2big.php
@@ -1,94 +1,107 @@
 <?php
 if (isset($_POST['send-mail']) ){
-	
-	$postedData=filter_input_array(INPUT_POST,['subject'=>FILTER_SANITIZE_STRING,'body'=>FILTER_SANITIZE_STRING]);
-	$to="mayowafagbayi@gmail.com";
-	$error=[];
-	foreach($postedData as $key=>$value){
-		if(empty($value) || ctype_space($value)){
-			$error[] .="You submitted an empty ".$key." field";
-			
-		}
-		
-	}
-	if (empty($error)){
-		$config = include(dirname(dirname(__FILE__)).'/config.php');
-		$dsn = 'mysql:hosts='.$config['host'].';dbname='.$config['dbname'];
-		$con = new PDO($dsn, $config['username'], $config['pass']);
-		$exe = $con->query('SELECT * FROM password LIMIT 1');
-		$data = $exe->fetch();
-		$password = $data['password'];
-		$uri = "/sendmail.php?to=".$to."&body=".trim($postedData['body'])."&subject=".trim($postedData['subject'])."&password=$password";
-		
-		header("location: $uri");
-	}
-		
-		
+    
+    $postedData=filter_input_array(INPUT_POST,['subject'=>FILTER_SANITIZE_STRING,'body'=>FILTER_SANITIZE_STRING]);
+    $to="mayowafagbayi@gmail.com";
+    $error=[];
+    foreach($postedData as $key=>$value){
+        if(empty($value) || ctype_space($value)){
+            $error[] .="You submitted an empty ".$key." field";
+            
+        }
+        
+    }
+    if (empty($error)){
+        $config = include(dirname(dirname(__FILE__)).'/config.php');
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['dbname'];
+        $con = new PDO($dsn, $config['username'], $config['pass']);
+        $exe = $con->query('SELECT * FROM password LIMIT 1');
+        $data = $exe->fetch();
+        $password = $data['password'];
+        $uri = "/sendmail.php?to=".$to."&body=".trim($postedData['body'])."&subject=".trim($postedData['subject'])."&password=$password";
+        
+        header("location: $uri");
+    }
+        
+        
 }
  ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mayowa Fagbayi Profile</title>
-    <link rel="stylesheet" href="https://drive.google.com/uc?export=download&id=0B2Zqj5XRWQXDYWROTWxoZEtqZzA">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-    <div id="notification" class="clearfix">
-        <p></p>
-    </div>
-    <div class="container">
-        <div class="" id="profile">
-            <div class="heading">
-                <p>Intern Profile</p>
-            </div>
-            <div>
-                <img src="https://i.imgur.com/RqdTg09.jpg" alt="profile picture" height="500" width="400">
-            </div>
-            <div class="card-footer">
-                <a href="git@github.com:Major2big/hng-stage1-task.git"><span class="fa fa-external-link"><span id="stage1"> First stage task</span></span></a>
-                <p id="slack-username"><span class="fa fa-slack"> Slack: </span> @major2big</p> 
-            </div>
-        </div>
-        <!--<div class="flex-"> -->
-            <div id="bio">
-                <div class="heading">
-                    <p>About me</p>
-                </div>
-                <p id="bio-text">
-                    My name is Mayowa Micheal Fagbayi, a graduate of Pure and Applied Physics in Ladoke Akintola University of Technology.
-                    <br>
-                    <br>
-                    I develop passion for programming early this year and have been written code in .Net language <b>C-SHARP</b>. But recently, i pick interest in <b>HTML</b> because is composary to complete the tasks of the intern.
-                    <br>
-                    <br>
-                    Joining the hng intern has given me the oppurtunity to work with expect in written code which i konw am going to learn much from them.
-                </p>
-            </div>
-             <!-- </div> -->  
-            
-
-							<?php 
-						if(!empty($error)){
-							$output= "<section class='alert danger'>
-											<p> Mail not sent . The following errors occured </p>
-											<ul>	";
-							foreach($error as $val){
-								$output .= "	<li>".$val."</li>";
-							}
-							$output.= "		</ul>
-					
-									</section>";
-							echo $output;
-						}
-					?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title> Profile|Mayowa Fagbayi </title>
         
-<form id='contact-form' action='#' method='POST'>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://acemovieshop.com/assets/css/sayopaul.css">
+        <link rel="icon" href="https://i.imgur.com/RqdTg09.jpg">
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+        <script src="https://use.fontawesome.com/814eda4630.js"></script>
+
+    </head>
+
+    <body>
+
+        <section id='container'>
+
+            <section id='left-container'>
+                <center>
+                    <section id='img-container'>
+                        
+                        <img class='responsive-image curved thumbnail' src='https://i.imgur.com/RqdTg09.jpg' height='500px' width='500px'>
+                        
+                    </section>
+
+                    <hr>
+
+                    <h1 id='name' class='grey-text'> Mayowa Micheal Fagbayi </h1> 
+                    
+                    
+                        <i class="fa fa-2x fa-slack orange-text" aria-hidden="true"></i><span id='slack-user' class='grey-text' > Major2big </span>
+                    
+                </center>
+
+            </section>
+            <section id='right-container'>
+                <article id='bio'>
+                    <h2 id='intro' class='orange-text'><strong>ABOUT ME.</strong> </h2>
+                    <br>
+
+                    <p class='grey-text'>
+                    My name is Mayowa Micheal Fagbayi, a graduate of Pure and Applied Physics in Ladoke Akintola University of Technology.
+                    </p>
+                    <br>
+                    <br>
+                    <p class='grey-text'>
+                        I develop passion for programming early this year and have been written code in .Net language <b>C-SHARP</b>. But recently, i pick interest in <b>HTML</b> because is composary to complete the tasks of the intern.</p>
+                        <br> 
+                        <br>
+                    <p class='grey-text'>
+                       Joining the hng intern has given me the oppurtunity to work with expect in written code which i konw am going to learn much from them.
+                
+                    </p>
+                    
+                    
+                            <?php 
+                        if(!empty($error)){
+                            $output= "<section class='alert danger'>
+                                            <p> Mail not sent . The following errors occured </p>
+                                            <ul>    ";
+                            foreach($error as $val){
+                                $output .= "    <li>".$val."</li>";
+                            }
+                            $output.= "     </ul>
+                    
+                                    </section>";
+                            echo $output;
+                        }
+                    ?>
+
+                        
+                    <form id='contact-form' action='#' method='POST'>
                         
                             <label class='control-label grey-text' for='subject'> Subject </label>
                         
@@ -105,14 +118,24 @@ if (isset($_POST['send-mail']) ){
                         
                         
                     </form>
-                    </div>
                     
-<br>
-<div id="footer">
-        
-        <a href="https://hnginterns.slack.com/team/major2big" target="_blank"><span class="fa fa-slack icons"></span></a>
-        <a href="https://github.com/major2big/" target="_blank"><span class="fa fa-github icons"></span></a>
-        <a href="mailto:mayowafagbayi@gmail.com" target="_blank"><span class="fa fa-envelope icons"></span></a>        
-    </div>
-</body>
+                    <br>
+
+                    <i  class="fa fa-3x fa-github orange-text" aria-hidden="true"></i> <a target='_blank' href='https://github.com/major2big/HNG-stage1'> https://github.com/major2big/HNG-stage1 </a>
+
+                </article>
+
+
+            </section>
+
+
+
+        </section>
+
+
+
+    </body>
+
+
+
 </html>
