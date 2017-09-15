72bd96878ae77c571e5e3cddb246c915d8b68bf0
Prosper Nweze <daponextraspp@gmail.com>
2017-09-15 12:37:13 +0100

    Fixed my page

--- profile/prosperoking/index.php
+++ profile/prosperoking/index.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
     <div class="container">
         <div class="profile">
