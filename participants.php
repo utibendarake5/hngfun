76359d659083d22e84e200d3ced71a9e45e6da2e
mattidungafa <mattidungafa@gmail.com>
2017-09-15 11:19:11 +0100

    openmattidungafa.php

--- participants.php
+++ participants.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $pageTitle = "Participants";
@@ -71,28 +72,7 @@ include('header.php');
 <!--  Each Participant starts here  -->
 
 
-               <!--  Each Participant starts here  -->
-               <div class="p-card">
-                   <div class="v-center">
-                       <div class="p-card-photo" style="background-image: url(https://github.com/NedyUdombat/Nedy/blob/master/my%20d.p.jpg?raw=true); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
-                       <div class="p-card-info">
-                           <a href="http://hng.fun/profile/nedyudombat.html">
-                               <!--Profile Link-->
-                               <h3 class="p-card-title">Edidiong Udombat</h3>
-                               <!--Profile Name-->
-                           </a>
-                           <p class="p-card-description">nedyudombat@gmail.com</p>
-                           <!--Email-->
-                       </div>
-                       <div class="p-team">
-                           <a href="#">
-                               <h3 class="p-card-title"> null </h3>
-                               <!--Team-->
-                           </a>
-                       </div>
-                   </div>
-               </div>
-               <!--  Each Participants ends here  -->
+              
 
 <!--  Each Participant starts here  -->
 <div class="p-card">
@@ -26447,3 +26427,29 @@ include('header.php');
                    </div>
                </div>
                <!--  Each Participants ends here  -->
+               <div class="p-card">
+                   <div class="v-center">
+                       <div class="p-card-photo" style="background-image: url("http://www.planwallpaper.com/static/images/blurry-lights-background.jpg")
+                       <div class="p-card-info">
+                           <a href="http://hng.fun/profile/mattidungafa/profile.html">
+                               <!--Profile Link-->
+                               <h3 class="p-card-title">Matthew Idugafa</h3>
+                               <!--Profile Name-->
+                           </a>
+                           <p class="p-card-description">mattidungafa@gmail.com</p>
+                           <!--Email-->
+                       </div>
+                       <div class="p-team">
+                           <a href="#">
+                               <h3 class="p-card-title"> None </h3>
+                               <!--Team-->
+                           </a>
+                       </div>
+                   </div>
+               </div>
+               ss<!--  Each Participants ends here  -->            
+
+
+
+
+                
