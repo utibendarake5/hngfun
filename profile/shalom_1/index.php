5365559076f1442936a5b8f1cc73507b8b83457d
Shalom101 <shalomizebhor@gmail.com>
2017-09-15 12:00:25 +0100

    Update index.php

--- profile/shalom_1/index.php
+++ profile/shalom_1/index.php
@@ -47,7 +47,6 @@
 
 
 
-    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
@@ -115,7 +114,5 @@
     </script>
 
 
-
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
   </body>
 </html>
