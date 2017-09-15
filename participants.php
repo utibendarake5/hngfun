05b087df7a46e3efbab82537fe62cea0845dbcf5
EnochMbaebie <chukwuemekambaebie@gmail.com>
2017-09-15 15:56:40 +0100

    updated the repo

--- participants.php
+++ participants.php
@@ -13,6 +13,22 @@ include('header.php');
         </div>
 
         <div class="p-card-container">
+            
+ <div class="p-card">
+    <div class="v-center">
+       <div class="p-card-photo" style="background-image: url(https://image.ibb.co/e0giMk/prolifichands.jpg); background-size: cover; background-repeat: no-repeat; background-position: center center; "></div>
+       <div class="p-card-info">
+           <a href="http://hng.fun/profile/enoch_mbaebie/index.html">                     
+               <!--Profile Link-->
+               <h3 class="p-card-title">Enoch Mbaebie</h3>
+               <!--Profile Name-->
+           </a>
+           <p class="p-card-description">chukwuemekambaebie@gmail.com</p>
+           <!--Email-->
+       </div>
+       <div class="p-team">
+           <a href="#">
+               <h3 class="p-card-title"> Yet to get a team </h3>
 
 
             <!--
